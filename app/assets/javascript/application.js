0fe88e3674ddf25f6bb2c838b60d321c7b9e1a4b
victor <victor!@>
2017-06-02 13:39:56 -0300

    drag and drop sprints

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -11,3 +11,39 @@ var requisicao = function(element){
   });
 };
 
+var attStatusSprint = function(sprintId, status){
+    var status = status.toUpperCase();
+    var id = sprintId;
+    var projetoId = $("#projetoId").val();
+
+    $.ajax({
+        url:"/scrumapp/sprint/atualizaStatusSprint",
+        type:"post",
+        data: {id: id, status: status},
+        success: function(data){
+            var dados = JSON.parse(data);
+            if(dados['result'] == 'true' || dados['result'] == true){
+                window.location="/scrumapp/projeto/visualizar?id="+projetoId;
+            }else{
+                bootbox.alert("Não foi possível alterar essa sprint, favor atualize a página, caso o erro persista contate o suporte!", function () {
+                    window.location="/scrumapp/projeto/visualizar?id="+projetoId;
+                });
+            }
+        }
+    });
+};
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    attStatusSprint(data, ev.target.id);
+}
