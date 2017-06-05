3f3384f1432517e11ff2b7d1002a21e29263ef6a
victor <victor!@>
2017-06-05 12:02:17 -0300

    model padrao

---
+++ model/Jogador.php
@@ -0,0 +1,15 @@
+<?php
+
+class Jogador
+{
+    //@Type:String
+    private $nome;
+    //@Type:Int
+    private $idade;
+    //@Type:String
+    private $posicao;
+    //@Type:Double
+    private $altura;
+    //@Type:Double
+    private $peso;
+}
